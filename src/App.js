6f43784e64dfed9cfe199b21d9799993b7395549
viki-korotkaya <viki.korotkaya@gmail.com>
2019-03-28 12:23:21 -0400

    Dynemic styles

--- src/App.js
+++ src/App.js
@@ -25,20 +25,24 @@ class App extends Component {
     persons.splice(personIndex, 1);
     this.setState({persons: persons})
   }
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-      {name: "Ana", age: 29}, 
-      {name: event.target.value, age: 33},
-      {name: "Sacha", age: 32},
-      ]
-    })
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({ persons: persons })
   }
 
   render() {
     const style = {
-      backgroundColor: "yellow",
+      backgroundColor: "green",
       font: 'inherit',
+      color: 'white',
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
@@ -52,17 +56,29 @@ class App extends Component {
                         click = {() => this.deletePersonHandler(index)}
                         name = {person.name} 
                         age = {person.age}
-                        key = {person.id}/>
+                        key = {person.id}
+                        changed = {(event) => this.nameChangeHandler(event, person.id)}/>
             })}
               
           </div> 
-      )
+      );
+      style.backgroundColor = 'red';
+    }
+
+    let classes = [];
+
+    if (this.state.persons.length <= 2){
+      classes.push('red');
+    }
+
+    if (this.state.persons.length <= 1){
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm react app</h1>
-        <p>This is really working</p>
+        <p className = {classes.join(' ')}>This is really working</p>
         <button
            style = {style}
            onClick={this.togglePersonHandler}>
