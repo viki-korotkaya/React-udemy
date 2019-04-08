47051332270d10943d23972f57ef378a0335fa0c
viki-korotkaya <viki.korotkaya@gmail.com>
2019-04-08 11:24:24 -0400

    Lifecycly hook continue

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css';
 
-const person = (props) => {
+class Person extends Component {
+	render(){
+		console.log('[Person.js] rendering.....');
 
-	console.log('[Person.js] rendering.....');
-	return(
-		<div className = {classes.Person}>
-			<p onClick={props.click}>I'm {props.name} and I'm {props.age} years old</p>
-			<p>{props.children}</p>
-			<input type="text" onChange={props.changed} value = {props.name} />
-		</div>
-	);
+		return(
+			<div className = {classes.Person}>
+				<p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old</p>
+				<p>{this.props.children}</p>
+				<input type="text" onChange={this.props.changed} value = {this.props.name} />
+			</div>
+		);
+	}
 };
 
-export default person;
+export default Person;
