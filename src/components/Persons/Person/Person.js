10c6b337c7818a6138222f0a9d675196c123dd53
viki-korotkaya <viki.korotkaya@gmail.com>
2019-04-11 09:41:13 -0400

    Array of elements instead of div root

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,13 +5,13 @@ class Person extends Component {
 	render(){
 		console.log('[Person.js] rendering.....');
 
-		return(
-			<div className = {classes.Person}>
-				<p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old</p>
-				<p>{this.props.children}</p>
-				<input type="text" onChange={this.props.changed} value = {this.props.name} />
-			</div>
-		);
+		return[
+			
+				<p key="i1" onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old</p>,
+				<p key="i2">{this.props.children}</p>,
+				<input key="i3" type="text" onChange={this.props.changed} value = {this.props.name} />
+			
+		];
 	}
 };
 
