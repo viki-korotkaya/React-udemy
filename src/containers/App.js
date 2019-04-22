542a814310f3d2b2111d93fba287bdd5676788c2
viki.korotkaya <viki.korotkaya@gmail.com>
2019-04-22 09:37:47 -0400

    Add HOC

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
   constructor(props){
@@ -76,7 +77,7 @@ class App extends Component {
 
     return (
       
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
       <button onClick = {() => this.setState({showCockpit: false})}>Remove Cockpit</button>
       {this.state.showCockpit ? (<Cockpit 
           showPersons={this.state.showPersons}
@@ -85,7 +86,7 @@ class App extends Component {
           persons = {this.state.persons} />) : null}
         
         {persons}
-      </div>
+      </WithClass>
       
         
     );
