95c02b17199170aba330f27ca1bfa1684eac364c
viki.korotkaya <viki.korotkaya@gmail.com>
2019-04-22 10:59:39 -0400

    Use Ref

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,14 +6,23 @@ import withClass from '../../../hoc/withClass';
 
 
 class Person extends Component {
-	render(){
+    componentDidMount() {
+        this.inputElement.focus();
+    }
+
+    render(){
 		console.log('[Person.js] rendering.....');
 
 		return (
 			<Auxiliary>
 				<p key="i1" onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old</p>
 				<p key="i2">{this.props.children}</p>
-				<input key="i3" type="text" onChange={this.props.changed} value = {this.props.name} />
+				<input
+                    key="i3"
+                    ref={(inputEl) => {this.inputElement = inputEl}}
+                    type="text"
+                    onChange={this.props.changed}
+                    value = {this.props.name} />
 
 			</Auxiliary>
 		);
